2fedd2b3ec7ca24c3e451bcf1c5be19011b5c7b2
Avinash Virupaksha <vavinash@zeomega.com>
2021-10-16 16:40:06 +0530

    Added Hello Apiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API view"""
+    
+    def get(self, request, format=None):
+        """returns a list of APIView features"""
+        an_apiview = [
+            'Uses Http methods as function (get, post, put, delete)',
+            'banana',
+            'orange'
+        ]
+        
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
