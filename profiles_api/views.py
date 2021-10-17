d28c6c0768c639e3cdb25252726ecb3e6282fb32
Avinash Virupaksha <vavinash@zeomega.com>
2021-10-17 17:53:48 +0530

    Added permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
-from profiles_api import serializers
+from profiles_api import serializers, models, permissions
+from rest_framework.authentication import TokenAuthentication
 
 
 class HelloApiView(APIView):
@@ -87,3 +88,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """handle delete of an object"""
         return Response({"http_method": 'Delete'})
+ 
+    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
