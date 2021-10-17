8c6833ee4444816271c065b9629aa982bcac99a0
Avinash Virupaksha <vavinash@zeomega.com>
2021-10-17 17:58:52 +0530

    Added searching functionality

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 from profiles_api import serializers, models, permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 
 class HelloApiView(APIView):
@@ -96,3 +97,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
